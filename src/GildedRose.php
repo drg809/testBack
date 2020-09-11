2477d76dcc329d552af99c8047f536b5ec95e977
Daniel Rodriguez <drg809@gmail.com>
2020-09-11 19:12:27 +0200

    php cs fixer configured and runned

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -2,36 +2,37 @@
 
 namespace Runroom\GildedRose;
 
-class GildedRose {
-
+class GildedRose
+{
     private array $items = [];
 
-    function __construct(array $items) {
+    public function __construct(array $items)
+    {
         $this->items = $items;
     }
 
-    function updateQuality() : void {
+    public function updateQuality() : void
+    {
         /** @var object $item */
         foreach ($this->items as  $item) {
             if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
                 if ($item->quality > 0) {
                     if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $this->setOperation(false,intval($item->quality),1);
-
+                        $item->quality = $this->setOperation(false, intval($item->quality), 1);
                     }
                 }
             } else {
                 if ($item->quality < 50) {
-                    $item->quality = $this->setOperation(true,intval($item->quality),1);
+                    $item->quality = $this->setOperation(true, intval($item->quality), 1);
                     if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
                         if ($item->sell_in < 11) {
                             if ($item->quality < 50) {
-                                $item->quality = $this->setOperation(true,intval($item->quality),1);
+                                $item->quality = $this->setOperation(true, intval($item->quality), 1);
                             }
                         }
                         if ($item->sell_in < 6) {
                             if ($item->quality < 50) {
-                                $item->quality = $this->setOperation(true,intval($item->quality),1);
+                                $item->quality = $this->setOperation(true, intval($item->quality), 1);
                             }
                         }
                     }
@@ -39,7 +40,7 @@ class GildedRose {
             }
 
             if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                $item->sell_in = $this->setOperation(false,intval($item->sell_in),1);
+                $item->sell_in = $this->setOperation(false, intval($item->sell_in), 1);
             }
 
             if ($item->sell_in < 0) {
@@ -47,22 +48,23 @@ class GildedRose {
                     if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
                         if ($item->quality > 0) {
                             if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $this->setOperation(false,intval($item->quality),1);
+                                $item->quality = $this->setOperation(false, intval($item->quality), 1);
                             }
                         }
                     } else {
-                        $item->quality = $this->setOperation(false,intval($item->quality),intval($item->quality));
+                        $item->quality = $this->setOperation(false, intval($item->quality), intval($item->quality));
                     }
                 } else {
                     if ($item->quality < 50) {
-                        $item->quality = $this->setOperation(true,intval($item->quality),1);
+                        $item->quality = $this->setOperation(true, intval($item->quality), 1);
                     }
                 }
             }
         }
     }
 
-    private function setOperation(bool $operation,int $op1,int $op2) : int {
+    private function setOperation(bool $operation, int $op1, int $op2) : int
+    {
         return $operation ? $op1 + $op2 : $op1 - $op2;
     }
 }
--- src/Item.php
+++ src/Item.php
@@ -2,20 +2,21 @@
 
 namespace Runroom\GildedRose;
 
-class Item {
-
+class Item
+{
     public string $name;
     public int $sell_in;
     public int $quality;
 
-    function __construct(string $name,int $sell_in,int $quality) {
+    public function __construct(string $name, int $sell_in, int $quality)
+    {
         $this->name = $name;
         $this->sell_in = $sell_in;
         $this->quality = $quality;
     }
 
-    public function __toString() : string {
+    public function __toString() : string
+    {
         return "{$this->name}, {$this->sell_in}, {$this->quality}";
     }
-
 }
